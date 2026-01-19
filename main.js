856754384f398870cbe998c6262f918462e25838
Adarsh yadav <ay1734861@gmail.com>
2026-01-19 16:15:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+const hamburger = document.getElementById('hamburgers');
+const navlist = document.getElementById('navlist');
+hamburger.addEventListener('click', () => navlist.classList.toggle('navlist-active'));
